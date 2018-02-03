0b963ff2f65bf9d187983203d9d3aa4472a487d1
MarcinGren <marcin.gren@gmail.com>
2018-02-03 14:13:53 +0100

    Xpath changes due to changes in Airbnb's DOM

--- AirbnbTest.py
+++ AirbnbTest.py
@@ -33,7 +33,7 @@ class AirbnbTest():
                 numberOfAllOffers = len(driver.find_elements(By.XPATH, xpathAllOffers))
             listOfAllOffers = []
             for offers in list(range(numberOfAllOffers)):
-                listing = driver.find_element_by_xpath("(%s//div[@class='_1rths372'])[%s]" % (xpathAllOffers, (offers + 1)))
+                listing = driver.find_element_by_xpath("(%s//div[@class='_19cnn6tn'])[%s]" % (xpathAllOffers, (offers + 1)))
                 listOfAllOffers.append(str(listing.get_attribute("innerText")))
 
             return listOfAllOffers
@@ -46,7 +46,7 @@ class AirbnbTest():
             listOfSuperhostsLinks = []
 
             for superhost in list(range(numberOfSuperhosts)):
-                xpathSuperhost = "//span[text()='Superhost']//parent::span//parent::div//parent::div//parent::a"
+                xpathSuperhost = "//span[text()='Superhost']//parent::span//parent::div//parent::div//parent::div//parent::a"
                 listing = driver.find_element_by_xpath("(%s)[%s]" % (xpathSuperhost, (superhost + 1)))
                 listOfSuperhostsLinks.append(listing.get_attribute("href"))
 
@@ -65,7 +65,7 @@ class AirbnbTest():
             for i in range(number):
                 hostButton = driver.find_element_by_xpath("//div[text()='Become a host']//parent::div//parent::button[@class='_hxsurab']")
                 hostButton.click()
-                hostaHomeButton = driver.find_element_by_xpath("(//li[@role='treeitem']//a)[1]")
+                hostaHomeButton = driver.find_element_by_xpath("(//li//a)[1]")
                 hostaHomeButton.click()
                 time.sleep(3)
                 driver.back()
@@ -130,15 +130,16 @@ class AirbnbTest():
         textBox = driver.find_element_by_xpath("//div[@class='_1qd1muk']//div[@class='_178faes']/input[@id='GeocompleteController-via-SearchBarV2-SearchBarV2']")
         textBox.send_keys("Vienna, Austria")
         textBox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
         #select date, can't use class, 6 buttons are the same, try with child text 'Dates'
-        datesButton = driver.find_element_by_xpath("//div[text()='Dates']//parent::button")
+        datesButton = driver.find_element_by_xpath("//span[text()='Dates']/parent::div/parent::button")
         datesButton.click()
         time.sleep(1)
 
-        checkInDate = driver.find_element_by_xpath("//div/div[3]//tr[2]/td[2]/button[@type='button']")
+        checkInDate = driver.find_element_by_xpath("//div/div[3]//tr[2]/td[2]")
         checkInDate.click()
-        checkOutDate = driver.find_element_by_xpath("//div/div[3]//tr[2]/td[5]/button[@type='button']")
+        checkOutDate = driver.find_element_by_xpath("//div/div[3]//tr[2]/td[5]")
         checkOutDate.click()
         applyButton = driver.find_element_by_xpath("//button[@type='button']/span[.='Apply']")
         applyButton.click()
@@ -175,6 +176,7 @@ class AirbnbTest():
         driver.refresh()
         thirdTry = saveAllLists()
         saveResults(secondTry, thirdTry, 2)
+        driver.quit()
 
 chrome = AirbnbTest()
 chrome.testVienna()
